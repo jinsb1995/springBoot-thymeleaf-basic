39d774233943237e8fc5ec76cd2adf9ecf9ab89e
jinsb1995 <jinsb1995@gmail.com>
2021-10-03 14:08:20 +0900

    header, footer 분리 method - layout, fragment

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -105,6 +105,18 @@ public class BasicController {
         return "basic/comments";
     }
 
+    @GetMapping("block")
+    public String block(Model model) {
+        addUsers(model);
+        return "basic/block";
+    }
+
+    @GetMapping("/javascript")
+    public String javascript(Model model) {
+        model.addAttribute("user", new User("UserA", 10));
+        addUsers(model);
+        return "basic/javascript";
+    }
 
 
     private void addUsers(Model model) {
